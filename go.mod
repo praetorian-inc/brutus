146dda2fc3517cedb7f74c96b0167a3b11106f36
Adam Crosser <adam.crosser@praetorian.com>
2026-09-10 16:33:44 +0700

    refactor(firebird): use github.com/nakagami/firebirdsql for authentication
    Fixes the credential path to go through the established client library
    instead of a hand-rolled wire handshake.

--- go.mod
+++ go.mod
@@ -66,9 +66,12 @@ require (
 	github.com/jcmturner/gofork v1.7.6 // indirect
 	github.com/jcmturner/goidentity/v6 v6.0.1 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/masterzen/simplexml v0.0.0-20190410153822-31eea3082786 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/nakagami/chacha20 v0.1.0 // indirect
+	github.com/nakagami/firebirdsql v0.9.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/projectdiscovery/wappalyzergo v0.2.17 // indirect
 	github.com/quic-go/quic-go v0.60.0 // indirect
@@ -79,6 +82,7 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	gitlab.com/nyarla/go-crypt v0.0.0-20160106005555-d9a5dc2b789b // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
--- internal/plugins/firebird/firebird.go
+++ internal/plugins/firebird/firebird.go
@@ -16,31 +16,25 @@ package firebird
 
 import (
 	"context"
-	"encoding/binary"
+	"database/sql"
 	"fmt"
-	"io"
 	"net"
+	"net/url"
 	"time"
+
+	_ "github.com/nakagami/firebirdsql"
 
 	"github.com/praetorian-inc/brutus/pkg/brutus"
 )
 
-const (
-	defaultPort  = "3050"
-	opConnect    = 1
-	opAccept     = 3
-	opReject     = 4
-	opAttach     = 19
-	opResponse   = 9
-	opCondAccept = 20
-	opAcceptData = 21
-)
+const defaultPort = "3050"
 
 var firebirdAuthIndicators = []string{
 	"your user name and password are not defined",
 	"login",
 	"password",
 	"sqlcode = -902",
+	"authentication error",
 }
 
 func init() {
@@ -58,126 +52,28 @@ func (p *Plugin) Test(ctx context.Context, target, username, password string,
 	defer func() { result.Duration = time.Since(start) }()
 
 	host, port := brutus.ParseTarget(target, defaultPort)
-	conn, err := brutus.DialWithProxy(ctx, "tcp", net.JoinHostPort(host, port), timeout, pluginCfg.ProxyURL)
+	u := url.UserPassword(username, password)
+	dsn := fmt.Sprintf("%s@%s/employee?timeout=%d", u.String(), net.JoinHostPort(host, port), int(timeout.Seconds()))
+	if int(timeout.Seconds()) <= 0 {
+		dsn = fmt.Sprintf("%s@%s/employee", u.String(), net.JoinHostPort(host, port))
+	}
+
+	db, err := sql.Open("firebirdsql", dsn)
 	if err != nil {
 		result.Error = classifyError(err)
 		return result
 	}
-	defer func() { _ = conn.Close() }()
-	_ = conn.SetDeadline(time.Now().Add(timeout))
+	defer func() { _ = db.Close() }()
+	db.SetConnMaxLifetime(timeout)
 
-	if err := writeConnect(conn); err != nil {
-		result.Error = brutus.WrapConnError(err)
-		return result
-	}
-	op, err := readOp(conn)
-	if err != nil {
-		result.Error = brutus.WrapConnError(err)
-		return result
-	}
-	if op == opReject {
-		result.Error = fmt.Errorf("connection error: firebird rejected protocol")
-		return result
-	}
-
-	if err := writeAttach(conn, username, password); err != nil {
-		result.Error = brutus.WrapConnError(err)
-		return result
-	}
-	op, blob, err := readResponse(conn)
-	if err != nil {
+	pingCtx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+	if err := db.PingContext(pingCtx); err != nil {
 		result.Error = classifyError(err)
 		return result
 	}
-	if op == opResponse {
-		if len(blob) >= 12 {
-			sqlcode := int32(binary.BigEndian.Uint32(blob[8:12]))
-			if sqlcode != 0 {
-				result.Error = classifyError(fmt.Errorf("firebird sqlcode %d", sqlcode))
-				return result
-			}
-		}
-		result.Success = true
-		return result
-	}
-	result.Error = fmt.Errorf("connection error: firebird op %d", op)
+	result.Success = true
 	return result
-}
-
-func writeConnect(w io.Writer) error {
-	// op_connect, op_attach, version, architecture, filename, protocols
-	var b []byte
-	b = appendInt(b, opConnect)
-	b = appendInt(b, opAttach)
-	b = appendInt(b, 3) // version 3
-	b = appendInt(b, 1) // generic arch
-	b = appendString(b, "")
-	b = appendInt(b, 1) // 1 protocol
-	b = appendInt(b, 10)
-	b = appendInt(b, 1)
-	b = appendInt(b, 2)
-	b = appendInt(b, 3)
-	b = appendInt(b, 2)
-	return writeAll(w, b)
-}
-
-func writeAttach(w io.Writer, user, pass string) error {
-	dpb := []byte{1}               // isc_dpb_version1
-	dpb = appendDPB(dpb, 28, user) // user_name
-	dpb = appendDPB(dpb, 29, pass) // password
-	var b []byte
-	b = appendInt(b, opAttach)
-	b = appendInt(b, 0)
-	b = appendString(b, "employee")
-	b = appendBytes(b, dpb)
-	return writeAll(w, b)
-}
-
-func appendInt(b []byte, v int) []byte {
-	var n [4]byte
-	binary.BigEndian.PutUint32(n[:], uint32(v))
-	return append(b, n[:]...)
-}
-
-func appendString(b []byte, s string) []byte {
-	return appendBytes(b, []byte(s))
-}
-
-func appendBytes(b, s []byte) []byte {
-	b = appendInt(b, len(s))
-	b = append(b, s...)
-	for len(b)%4 != 0 {
-		b = append(b, 0)
-	}
-	return b
-}
-
-func appendDPB(b []byte, typ byte, val string) []byte {
-	b = append(b, typ, byte(len(val)))
-	return append(b, val...)
-}
-
-func readOp(r io.Reader) (int, error) {
-	var n uint32
-	if err := binary.Read(r, binary.BigEndian, &n); err != nil {
-		return 0, err
-	}
-	return int(n), nil
-}
-
-func readResponse(r io.Reader) (int, []byte, error) {
-	op, err := readOp(r)
-	if err != nil {
-		return 0, nil, err
-	}
-	buf := make([]byte, 32)
-	n, _ := io.ReadFull(r, buf)
-	return op, buf[:n], nil
-}
-
-func writeAll(w io.Writer, b []byte) error {
-	_, err := w.Write(b)
-	return err
 }
 
 var classifyError = brutus.NewClassifier(firebirdAuthIndicators)

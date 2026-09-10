692b1ee641009ea90eff69a518320a2c4d479b1c
Adam Crosser <adam.crosser@praetorian.com>
2026-09-10 15:48:02 +0700

    feat(protocol): add support for OPC UA
    Fixes ENG-7920

--- internal/plugins/init.go
+++ internal/plugins/init.go
@@ -31,6 +31,7 @@ import (
 	_ "github.com/praetorian-inc/brutus/internal/plugins/mssql"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/mysql"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/neo4j"
+	_ "github.com/praetorian-inc/brutus/internal/plugins/opcua"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/oracle"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/pop3"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/postgresql"

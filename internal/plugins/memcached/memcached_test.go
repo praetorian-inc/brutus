ac025e5d1b7fbc30c6067a74d70631fe640c9cd3
Adam Crosser <adam.crosser@praetorian.com>
2026-09-10 15:48:02 +0700

    feat(protocol): add support for Memcached
    Fixes ENG-7911

---
+++ internal/plugins/memcached/memcached_test.go
@@ -0,0 +1,42 @@
+package memcached
+
+import (
+	"context"
+	"io"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/praetorian-inc/brutus/pkg/brutus"
+)
+
+func TestPlugin_Name(t *testing.T) {
+	assert.Equal(t, "memcached", (&Plugin{}).Name())
+}
+
+func TestPlugin_Test_ConnectionRefused(t *testing.T) {
+	r := (&Plugin{}).Test(context.Background(), "127.0.0.1:1", "u", "p", 2*time.Second, brutus.PluginConfig{})
+	assert.False(t, r.Success)
+	assert.Contains(t, r.Error.Error(), "connection error")
+}
+
+func TestPlugin_CheckUnauth_Open(t *testing.T) {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+	defer func() { _ = ln.Close() }()
+	go func() {
+		conn, err := ln.Accept()
+		if err != nil {
+			return
+		}
+		defer func() { _ = conn.Close() }()
+		buf := make([]byte, 64)
+		_, _ = conn.Read(buf)
+		_, _ = io.WriteString(conn, "VERSION 1.6.0\r\n")
+	}()
+	r := (&Plugin{}).CheckUnauth(context.Background(), ln.Addr().String(), 3*time.Second, brutus.PluginConfig{})
+	assert.True(t, r.Success)
+}

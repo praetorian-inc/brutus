b22cf32fce057a7604dca5fd8ed7041a3f6989ea
Adam Crosser <adam.crosser@praetorian.com>
2026-09-10 15:48:02 +0700

    feat(protocol): add support for rsync
    Fixes ENG-7919

--- internal/plugins/init.go
+++ internal/plugins/init.go
@@ -36,6 +36,7 @@ import (
 	_ "github.com/praetorian-inc/brutus/internal/plugins/postgresql"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/rdp"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/redis"
+	_ "github.com/praetorian-inc/brutus/internal/plugins/rsync"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/smb"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/smtp"
 	_ "github.com/praetorian-inc/brutus/internal/plugins/snmp"
